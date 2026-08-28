5b67d03bb8f8f56c44964b145b9ed178e51d5bfe
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-08-28 09:22:26 +0900

    [Passed] 변수 값 교체하기 6 (112ms, 9MB)

---
+++ trail1/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 2, b = 5;
+        int temp = b;
+        b = a;
+        a = temp;
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
