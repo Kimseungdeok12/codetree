0b3a98c697f98f4d7138726a1ad0686fab7a3e08
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-07 11:44:22 +0900

    [Passed] 감소시키며 출력하기 2 (119ms, 10MB)

---
+++ trail1/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 26;
+
+        while (a >= 10) {         
+            System.out.print(a + " ");  
+            a--;                 
+        }
+    }
+}
