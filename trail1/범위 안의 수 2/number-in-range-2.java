81ca6a50a53763d12c254841e670087462ec402a
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-08 14:43:26 +0900

    [Passed] 범위 안의 수 2 (149ms, 10MB)

---
+++ trail1/범위 안의 수 2/number-in-range-2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = 10;
+
+        int sumVal = 0;
+        int cnt = 0;
+
+        for (int i = 1; i <= 10; i++) {
+
+            int a = sc.nextInt();
+
+            if (a >= 0 && a <= 200) {
+                sumVal += a;
+                cnt++;
+            }
+        }
+        System.out.print(sumVal + " ");
+        System.out.printf("%.1f",(double)sumVal / cnt);
+    }
+}
