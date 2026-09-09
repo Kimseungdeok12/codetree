d88951a682d43df702e4b3b0b345e00c8c86cf1d
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-09 14:31:33 +0900

    [Passed] 모두 3의 배수 (131ms, 10MB)

---
+++ trail1/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean st = true;
+        
+        for (int i = 1; i <= 5; i++) {
+            int a = sc.nextInt();
+            if (a % 3 != 0) {
+                st = false;
+            }
+        }
+        if (st) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
