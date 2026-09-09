172d9c021da32d486cf78a246256cce3eb7f9312
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-09 09:53:48 +0900

    [Passed] 1부터의 합 (129ms, 10MB)

---
+++ trail1/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int a = 0;
+
+        for (int i = a; i <= 100; i++) {
+            a += i;
+            
+            if (a >= n) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
