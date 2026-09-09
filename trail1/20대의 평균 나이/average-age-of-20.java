7abafc50b714b3e4dbe43ca36196c794db702c1a
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-09 12:07:37 +0900

    [Passed] 20대의 평균 나이 (132ms, 10MB)

---
+++ trail1/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,23 @@
+ import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cnt = 0;
+        int sum = 0;
+
+        while (true) {
+            int n = sc.nextInt();
+
+            if (n >= 20 && n < 30) {
+                cnt++;
+                sum += n;
+            } else {
+                System.out.printf("%.2f",(double) sum/cnt);
+                break;
+            }
+
+        }
+    }
+}
