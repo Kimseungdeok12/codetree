514c04c8ed7b24458ee5bf66fd4bce80696f6992
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-09 09:35:58 +0900

    [Passed] 조건에 부합하는 수 (130ms, 10MB)

---
+++ trail1/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0 && i % 4 != 0) {
+                continue;
+            }
+
+            if ((i / 8) % 2 == 0) {
+                continue;
+            }
+
+            if (i % 7 < 4) {
+                continue;
+            }
+
+            System.out.print(i + " ");
+        }
+    }
+}
