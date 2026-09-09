37984d77bf4aa3d6452e73fb4b475df8817eeabd
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-09 14:21:47 +0900

    [Passed] 공약수의 유무 (134ms, 10MB)

---
+++ trail1/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,26 @@
+ import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        boolean st = false;
+
+        for (int i = a; i <= b; i++) {
+            if (1920 % i == 0 && 2880 % i == 0) {
+                st = true;
+                break;
+            }
+        }
+
+        if (st) {
+            System.out.print("1");
+        } else {
+            System.out.print("0");
+        }
+
+    }
+}
