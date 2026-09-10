af07895518f6f59d849cc3f09574a2bf77e0beda
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-10 11:23:13 +0900

    [Passed] 별표 출력하기 10 (154ms, 10MB)

---
+++ trail1/별표 출력하기 10/print-star-10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n*2; i++) {
+            if (i % 2 == 1) {
+                for(int j = 1; j <= n - (i - 1) / 2; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            } else {
+                for(int j = 1; j <= 1 + (i / 2); j++) {
+                    System.out.print("* ");
+                }
+                System.out.println(); 
+            }
+        }
+    }
+}
