82f81dbb9d5a1b16af47b033f3ff8848dff389d4
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-10 17:44:57 +0900

    [Passed] 19단 출력 (153ms, 11MB)

---
+++ trail1/19단 출력/nineteen-times-table.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 19; i++) {
+            for (int j = 1; j <= 19; j++) {
+                System.out.print(i + " * " + j + " = " + i*j);
+                if (j == 19) {
+                    System.out.println();
+                } else if (j % 2 == 1) {
+                    System.out.print(" / ");
+                } else {
+                    System.out.println();
+                }
+            }
+        } 
+    }
+}
