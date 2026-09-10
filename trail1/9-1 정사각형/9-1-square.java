ce86ccb85f86e69d1124e9725d54647892911078
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-10 15:32:23 +0900

    [Passed] 9-1 정사각형 (132ms, 10MB)

---
+++ trail1/9-1 정사각형/9-1-square.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 9;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(cnt);
+                if (cnt == 1) {
+                    cnt = 9;
+                } else {
+                    cnt--;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
