aa9b4371d503f137dd614f9e3b734b27bdfdeae8
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-17 14:35:15 +0900

    [Passed] N개의 숫자 중 최소 (147ms, 10MB)

---
+++ trail1/N개의 숫자 중 최소/min-of-n-num.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == min) {
+                cnt++;
+            }
+        }
+
+        System.out.println(min + " " + cnt);
+    }
+}
+
