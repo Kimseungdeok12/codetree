af0bc4e3e3126d8c09a01b27c203bc4c21f984f4
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-17 09:17:57 +0900

    [Passed] 100 도달하기 (130ms, 10MB)

---
+++ trail1/100 도달하기/reach-100.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[100];
+
+        arr[0] = 1;
+        arr[1] = sc.nextInt();
+
+        for (int i = 0; i < 100; i++) {
+            if (i < 2) {
+                System.out.print(arr[i] + " ");
+            } else {
+                arr[i] = arr[i-1] + arr[i-2];
+                System.out.print(arr[i] + " ");
+            }
+
+            if (arr[i] > 100) {
+                break;
+            }    
+        }
+    }   
+}
