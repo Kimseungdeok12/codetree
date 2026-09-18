11b1d88c69833231811b189b6216a26fa2082014
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-18 09:54:17 +0900

    [Passed] 배열의 평균 (134ms, 10MB)

---
+++ trail1/배열의 평균/ave-of-array.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[2][4];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            int sum = 0;
+            for (int j = 0; j < 4; j++) {
+                sum += arr[i][j];
+            }
+
+            System.out.printf("%.1f ", (double) sum / 4);
+        }
+
+        System.out.println();
+
+        int summ = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int sum = 0;
+            for (int j = 0; j < 2; j++) {
+                sum += arr[j][i];
+                summ += arr[j][i];
+            }
+
+            System.out.printf("%.1f ", (double) sum / 2);
+        }
+
+        System.out.println();
+
+        System.out.printf("%.1f ", (double) summ / 8);
+
+    }
+}
+
