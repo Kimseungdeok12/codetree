59641583d5c801ab7e374b0f59142f2a22c20736
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-18 10:34:31 +0900

    [Passed] 아래로 사각형 채우기 (133ms, 10MB)

---
+++ trail1/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            int a = i + 1;
+            for (int j = 0; j < n; j++) {
+                System.out.print(a + j * n + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
