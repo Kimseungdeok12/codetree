7aa72a3025738f4395bdb1096f0aa28b08a60a13
Kimseungdeok12 <156771734+Kimseungdeok12@users.noreply.github.com>
2026-09-21 16:45:13 +0900

    [Passed] 문자열 전부 붙이기 (142ms, 10MB)

---
+++ trail1/문자열 전부 붙이기/paste-all-string.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String str = "";
+
+        for(int i = 0; i < n; i++) {
+            String a = sc.next();
+            str+= a;
+        }
+
+        System.out.print(str);
+    }
+}
